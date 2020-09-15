f1974ee1c7409e7a51a5bd1d6f2e346969be8c47
Josef Richter <josef.richter@me.com>
2020-09-15 15:22:10 +0200

    various exercises added

--- src/webdev/core.clj
+++ src/webdev/core.clj
@@ -2,7 +2,8 @@
   (:require [ring.adapter.jetty :as jetty]
             [ring.middleware.reload :refer [wrap-reload]]
             [compojure.core :refer [defroutes GET]]
-            [compojure.route :refer [not-found]]))
+            [compojure.route :refer [not-found]]
+            [ring.handler.dump :refer [handle-dump]]))
 
 (defn greet [req]
     {:status 200
@@ -15,11 +16,45 @@
    :body "Goodbye, cruel world!"
    :headers {}})
 
+(defn about [req]
+  {:status 200
+   :body "I am Josef and I'm learning me some Clojure"
+   :headers {}})
+
+(defn yo [req]
+  {:status 200
+   :body (str "Yo! " (:name (:route-params req)) "!")
+   :headers {}})
+
+(def ops
+  {"+" +
+   "-" -
+   "*" *
+   ":" /})
+
+(defn calc [req]
+  (let [a (Integer. (get-in req [:route-params :a]))
+        b (Integer. (get-in req [:route-params :b]))
+        op (get-in req [:route-params :op])
+        f (get ops op)]
+    (if f
+      {:status 200
+       :body (str (f a b))
+       :headers {}}
+      {:status 404
+       :body (str "Unknown operator: " op)
+       :headers {}})
+    )
+)
+
 (defroutes app
   (GET "/" [] greet)
-  (GET "/goodbye" [] goodbye) 
-  (not-found "Page not found.")
-  )
+  (GET "/goodbye" [] goodbye)
+  (GET "/about" [] about)
+  (GET "/yo/:name" [] yo)
+  (GET "/calc/:a/:op/:b" [] calc)
+  (GET "/request" [] handle-dump)
+  (not-found "Page not found."))
 
 (defn -main [port]
   (jetty/run-jetty app {:port (Integer. port)}))
